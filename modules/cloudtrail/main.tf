88ab7866eb0dab7c2bf3dd75b9186e146adf0c03
narendiransankar <narendiransankar1699@gmail.com>
2025-07-27 13:11:38 +0530

    Update main.tf

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -1,7 +1,38 @@
 resource "aws_s3_bucket" "cloudtrail" {
   bucket = "${var.environment}-cloudtrail-logs-hcl-bayer"
   force_destroy = true
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AWSCloudTrailWrite"
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action = "s3:PutObject"
+        Resource = "${aws_s3_bucket.cloudtrail.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      },
+      {
+        Sid       = "AWSCloudTrailAclCheck"
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.cloudtrail.arn
+      }
+    ]
+  })
 }
+
+data "aws_caller_identity" "current" {}
+
 
 resource "aws_cloudtrail" "main" {
   name                          = "${var.environment}-trail"
