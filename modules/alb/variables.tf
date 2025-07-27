60f40b5cab3f4b74f949eeeb77b7b48c61ae9afc
narendiransankar <narendiransankar1699@gmail.com>
2025-07-27 08:50:41 +0530

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -10,3 +10,23 @@ variable "vpc_id" {
     description = "The VPC ID"
     type = string
 }
+
+variable "certificate_arn" {
+  description = "ARN of the ACM certificate for ALB HTTPS listener"
+  type        = string
+}
+
+variable "cognito_user_pool_arn" {
+  description = "Cognito User Pool ARN for authentication"
+  type        = string
+}
+
+variable "cognito_user_pool_client_id" {
+  description = "Cognito User Pool App Client ID for authentication"
+  type        = string
+}
+
+variable "cognito_user_pool_domain" {
+  description = "Cognito User Pool domain prefix for authentication"
+  type        = string
+}
