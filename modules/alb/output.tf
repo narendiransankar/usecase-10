4dbf6a2b982149ca69cd91f60484aad0c7e398f5
narendiransankar <narendiransankar1699@gmail.com>
2025-07-27 08:51:48 +0530

    Update output.tf

--- modules/alb/output.tf
+++ modules/alb/output.tf
@@ -1,13 +1,22 @@
 output "alb_sg_id" {
     value = aws_security_group.alb.id
 }
-output "patient_target_group_arn" {
-value  = aws_lb_target_group.patient_service.arn
-}
-output "appointment_target_group_arn" {
-    value = aws_lb_target_group.appointment_service.arn
-}
+
 output "alb_end_point_anme" {
   description = "The DNS name of the Load Balancer"
   value       = aws_lb.main.dns_name
+}
+output "alb_dns_name" {
+  description = "DNS name of the ALB"
+  value       = aws_lb.main.dns_name
+}
+
+output "patient_tg_arn" {
+  description = "Target Group ARN for Patient service"
+  value       = aws_lb_target_group.patient_service.arn
+}
+
+output "appointment_tg_arn" {
+  description = "Target Group ARN for Appointment service"
+  value       = aws_lb_target_group.appointment_service.arn
 }
